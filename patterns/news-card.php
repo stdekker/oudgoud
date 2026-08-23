56f083eb976f4043b716f74c43e45a797de1f705
Stijn Dekker <stijndekker@protonmail.com>
2026-08-23 14:35:17 +0200

    Generic news card pattern

---
+++ patterns/news-card.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: News card
+ * Slug: oudgoud/news-card
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"tagName":"article","className":"news-card","layout":{"type":"default"}} -->
+<article class="wp-block-group news-card">
+	<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","sizeSlug":"large","className":"news-card__image"} /-->
+	<!-- wp:group {"className":"news-card__body","layout":{"type":"default"}} -->
+	<div class="wp-block-group news-card__body">
+		<!-- wp:post-title {"isLink":true,"level":2,"className":"news-card__title"} /-->
+		<!-- wp:group {"className":"news-card__meta","layout":{"type":"default"}} -->
+		<div class="wp-block-group news-card__meta">
+			<!-- wp:post-date {"format":"j F Y","isLink":true,"className":"news-card__date"} /-->
+			<!-- wp:post-terms {"term":"category","className":"news-card__categories"} /-->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:post-excerpt {"moreText":"Lees verder","excerptLength":34,"className":"news-card__excerpt"} /-->
+	</div>
+	<!-- /wp:group -->
+</article>
+<!-- /wp:group -->
